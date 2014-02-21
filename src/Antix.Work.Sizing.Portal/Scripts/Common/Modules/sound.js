da10b66c67d1ad7146e5b0bc2f55461e52820b3d
Anthony Johnston <aj@antix.co.uk>
2014-02-21 12:50:08 +0000

    catch errors in sound and disable (safari)

--- src/Antix.Work.Sizing.Portal/Scripts/Common/Modules/sound.js
+++ src/Antix.Work.Sizing.Portal/Scripts/Common/Modules/sound.js
@@ -2,26 +2,40 @@
 
     var Sound = function (mp3) {
 
-        var audio = new Audio(mp3),
+        var audio,
             loops = 1;
 
+        try {
+
+            audio = new Audio(mp3);
+        } catch (ex) {
+            audio = false;
+        }
+
         this.init = function () {
-            audio.play();
-            audio.pause();
+            if (audio) {
+
+                audio.play();
+                audio.pause();
+            };
+
             return this;
         };
 
         this.play = function () {
-            audio.play();
+            if (audio) {
+                audio.play();
+            }
+
             return this;
         };
 
-        this.withLoop = function(count) {
+        this.withLoop = function (count) {
             loops = count;
             return this;
         };
 
     };
-    
+
     return Sound;
 });
